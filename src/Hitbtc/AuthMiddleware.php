dce80ef48a7efef20078f38c4a361f931021fc76
Vytaliy Makovyak <thisinfoms@gmail.com>
2022-08-17 03:15:25 +0300

    Auth api v3 flow

--- src/Hitbtc/AuthMiddleware.php
+++ src/Hitbtc/AuthMiddleware.php
@@ -24,22 +24,9 @@ class AuthMiddleware
     public function __invoke(callable $handler)
     {
         return function (RequestInterface $request, array $options) use (&$handler) {
-            $queryString = $request->getUri()->getQuery();
-            $queryParts = \GuzzleHttp\Psr7\Query::parse($queryString);
-
-            $queryParts['apikey'] = $this->publicKey;
-            $queryParts['nonce'] = $this->getNonce();
-
-            $queryString = \GuzzleHttp\Psr7\Query::build($queryParts);
-            $request = $request->withUri($request->getUri()->withQuery($queryString));
-
-            $message = $request->getUri()->getPath(). '?' . $queryString . $request->getBody();
-            $sign = strtolower(hash_hmac('sha512', $message, $this->secretKey));
-
             $request = $request
-                ->withAddedHeader('Api-Signature', $sign)
-                ->withAddedHeader('User-Agent', 'Hitbtc PHP Client')
-                ;
+                ->withAddedHeader('Authorization', 'Basic ' . base64_encode($this->publicKey . ':' . $this->secretKey))
+                ->withAddedHeader('User-Agent', 'Hitbtc PHP Client');
 
             return $handler($request, $options);
         };
